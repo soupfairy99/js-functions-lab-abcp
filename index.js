90eefbe45851d1ec964900000b969077dfe073a4
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-13 19:44:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ function happyHolidays()
 }
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
+  
+}
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
 }
